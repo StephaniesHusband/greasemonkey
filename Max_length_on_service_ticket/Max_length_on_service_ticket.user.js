0ee889cee66df1fb36a10e008d35f37781ddd440
StephaniesHusband <StephaniesHusband@gmail.com>
2013-06-03 10:18:21 -0500

    new script

---
+++ Max_length_on_service_ticket/Max_length_on_service_ticket.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        Max length on service ticket
+// @namespace   fedex.scotterwin
+// @description set max length
+// @include     https://sso.secure.fedex.com/techweb/srvpro.jsp
+// @version     1
+// ==/UserScript==
+
+$(document).ready(function() {
+   $("textarea[name='notes']").maxLength=255
+   $("select[name='fxStationID']").selectedIndex = 52;
+   $("input[name='equip_loc']").value = "3860 Forest Hill Irene Rd, Ste 107";
+});
