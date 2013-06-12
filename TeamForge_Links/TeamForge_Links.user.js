a6773347726d488ad4055a77b3f8b5bfae6f2fba
StephaniesHusband <StephaniesHusband@gmail.com>
2013-06-12 12:03:41 -0500

    updated for 1440

--- TeamForge_Links/TeamForge_Links.user.js
+++ TeamForge_Links/TeamForge_Links.user.js
@@ -19,9 +19,9 @@
 //
 // ==/UserScript==
 
-var CUR_VERSION = "1420";
+var CUR_VERSION = "1440";
 var DOC_ROOT = "http://itg.prod.fedex.com/sf/docman/do/listDocuments/projects.sqa_senseaware/docman.root";
-var VER_BASE = ".wsaw1420";
+var VER_BASE = ".wsaw1440_ol1308_aug_2013_dl";
 var VER_ROOT = DOC_ROOT + VER_BASE;
 var JOB_AID = DOC_ROOT + ".templates";
 var DEV_DIR = DOC_ROOT + VER_BASE + ".development";
