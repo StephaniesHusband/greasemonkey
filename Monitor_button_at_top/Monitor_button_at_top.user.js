66b0f70f66e97250a4bf46b33dfe590740eda426
StephaniesHusband <StephaniesHusband@gmail.com>
2013-06-24 17:04:43 -0500

    new script to put monitor button on top toolbar

---
+++ Monitor_button_at_top/Monitor_button_at_top.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        Monitor button at top
+// @namespace   fedex.scotterwin
+// @include     http://itg.prod.fedex.com/sf/docman/do/listDocuments*
+// @require     https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js
+// @version     1
+// ==/UserScript==
+
+
+$(document).ready(function() {
+   $("table#documentList tr.ContainerHeader td div:first").append($("#_SfButton_monitor").clone());
+});
